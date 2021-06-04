d052b33ce9c7388b86fc31570d1c6e7531d95b2d
Antoine G <wakeup@sleepingpanda.games>
2021-06-04 11:27:10 +0200

    Added a music loop

---
+++ Assets/Scripts/gameMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMusic : MonoBehaviour
+{
+    private GameObject[] music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = GameObject.FindGameObjectsWithTag("GameMusic");
+        Destroy(music[1]);
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
